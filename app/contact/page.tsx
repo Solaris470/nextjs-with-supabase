3f4333dafb13dcc121df9fe28295f82f07fbd936
72123 <88994370+koonguy7838@users.noreply.github.com>
2024-10-02 00:28:17 +0700

    updata-contact
    แก้บัค ดึงข้อมูลจาก data

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,74 +1,88 @@
 export default function Contact(){
+
+    const dataContact = [
+        {
+            id: 1,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 2,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 3,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 4,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 5,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 6,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 7,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        },{
+            id: 8,
+            contact_name: 'title.satsuke',
+            image: '/image/satuske.jpg',
+            role: 'Tester',
+            email: 'title.satsuke@test.com'
+        }
+    ]
+
     return (
-        <body>
         <div className="bg-[#f5f6fa]">
             <div>
-                <h1 className="pt-12 pl-12 text-start text-4xl font-medium">Contact</h1>
-                <div className="grid grid-cols-3 gap-4 text-center">
-
-                    <div className="flex justify-center">
-                        <div className="max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <img className="rounded-t-lg object-cover h-72 w-full" src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/460337628_815035277512139_2177229483913410069_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHLCIDJvXnbSFl5tJFIuQ3v98tQE2VTGWP3y1ATZVMZYwVW1Xr-hefOzxSnDF46fadoggB3ztEiuduylLrlM0N1&_nc_ohc=OkfddWhlB48Q7kNvgH2gNVy&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=ADVvYsEOisTNL6M0guj89Yo&oh=00_AYBiIQuWWp-NhTwLE-Cz286dn8uXPH8jnvT_5-k1PS08iA&oe=66F2F36D" alt="" />
-                            </a>
-                            <div className="p-5">
-                                <a href="#">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                                </a>
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                <a href="#" className="inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                                    Read more
-                                    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center">
-                        <div className="max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <img className="rounded-t-lg object-cover h-72 w-full" src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/460337628_815035277512139_2177229483913410069_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHLCIDJvXnbSFl5tJFIuQ3v98tQE2VTGWP3y1ATZVMZYwVW1Xr-hefOzxSnDF46fadoggB3ztEiuduylLrlM0N1&_nc_ohc=OkfddWhlB48Q7kNvgH2gNVy&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=ADVvYsEOisTNL6M0guj89Yo&oh=00_AYBiIQuWWp-NhTwLE-Cz286dn8uXPH8jnvT_5-k1PS08iA&oe=66F2F36D" alt="" />
-                            </a>
-                            <div className="p-5">
-                                    <a href="#">
-                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                                    </a>
-                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                    <a href="#" className="inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                                        Read more
-                                        <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                        </svg>
-                                    </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-center">
-                        <div className="max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                                <img className="rounded-t-lg object-cover h-72 w-full" src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/460337628_815035277512139_2177229483913410069_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHLCIDJvXnbSFl5tJFIuQ3v98tQE2VTGWP3y1ATZVMZYwVW1Xr-hefOzxSnDF46fadoggB3ztEiuduylLrlM0N1&_nc_ohc=OkfddWhlB48Q7kNvgH2gNVy&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=ADVvYsEOisTNL6M0guj89Yo&oh=00_AYBiIQuWWp-NhTwLE-Cz286dn8uXPH8jnvT_5-k1PS08iA&oe=66F2F36D" alt="" />
-                            </a>
-                            <div className="p-5">
-                                <a href="#">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                                </a>
-                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                                <a href="#" className="inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                                    Read more
-                                    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                <h1 className="pt-12 pl-12 pb-2 text-start text-4xl font-medium">Contact</h1>
+                <div className="grid grid-cols-4 gap-4 text-center">
+                        {
+                            dataContact.map((dataContacts) => (
+                                <div className="justify-center">
+                                    <div className="max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                        <a href="#">
+                                            <img className="rounded-t-lg object-cover h-72 w-full" src={dataContacts.image} alt="" />
+                                        </a>
+                                        <div className="p-5">
+                                            <a href="#">
+                                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{dataContacts.contact_name}</h5>
+                                            </a>
+                                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{dataContacts.email}</p>
+                                            <a href="#" className="inline-flex items-center text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                                                Read more
+                                                <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </div> 
+                                </div>
+                            ))
+                        }
                     
                 </div>
             </div>
         </div>
-        </body>
     )
 }
