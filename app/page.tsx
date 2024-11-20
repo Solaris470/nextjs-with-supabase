80de3f824dfffb7258f1cd3d1dd7e44c41a5beaf
Atit Sontirak <ifnet470@gmail.com>
2024-11-20 13:39:08 +0700

    config middleware route

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 export default async function Index() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      This is Home page <a href="/sign-in" className="text-blue-500">Please Login </a>
     </>
   );
 }
