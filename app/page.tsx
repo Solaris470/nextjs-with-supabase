38721692a1809ed8e1d2b6cada4d980806fb6286
Solaris470 <77920188+Solaris470@users.noreply.github.com>
2025-02-16 16:50:26 +0700

    ไม่ใช้ Please login

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
 
 export default async function Index() {
+  const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <>
       This is Home page <a href="/sign-in" className="text-blue-500">Please Login </a>
